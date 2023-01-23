3b2c296a3ffadd48cd8fa9aaf91c8059b0ec8693
Demian Skoglöv <demian.skoglow@gmail.com>
2023-01-23 15:51:07 +0100

    Movement funkar men suger

--- Assets/Demian Prog/MovementPlayer.cs
+++ Assets/Demian Prog/MovementPlayer.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using UnityEngine.UI;
+using Vector3 = UnityEngine.Vector3;
 
 public class MovementPlayer : MonoBehaviour
 {
+
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float acceleration;
+    private Vector3 deltaMovement;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,18 @@ public class MovementPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        deltaMovement.x = Input.GetAxis("Horizontal");
+        deltaMovement.y = 0;
+        deltaMovement.z = Input.GetAxis("Vertical");
         
+        Debug.Log(deltaMovement.x);
+        Debug.Log(deltaMovement.z);
+        
+        MovePlayer(deltaMovement * moveSpeed * Time.deltaTime);
+    }
+
+    void MovePlayer(Vector3 deltaMovement)
+    {
+        rb.MovePosition(transform.position + deltaMovement);
     }
 }
