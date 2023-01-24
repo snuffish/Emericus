534399d63b1051b762ac518b6c26be0ac5306b88
Demian Skoglöv <demian.skoglow@gmail.com>
2023-01-24 11:44:04 +0100

    Kamera Script fungerar
    man kan nu applicera ett kamera script som möjliggör kolla-runt-funktionen

---
+++ Assets/Demian Prog/Scripts/CameraPos.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPos : MonoBehaviour
+{
+
+    [SerializeField] private Transform cameraPosition;
+
+    // Update is called once per frame
+    void Update() {
+
+        transform.position = cameraPosition.position;
+    }
+}
