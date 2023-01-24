f59096b3fce26891942d8c92f053c24331d8fd2c
Demian Skoglöv <demian.skoglow@gmail.com>
2023-01-24 13:03:49 +0100

    Movement är klart?. Tweaking behövs

--- Assets/Demian Prog/Scripts/MovementPlayer.cs
+++ Assets/Demian Prog/Scripts/MovementPlayer.cs
@@ -1,39 +1,104 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
+using Unity.VisualScripting.FullSerializer.Internal;
 using UnityEngine;
 using UnityEngine.UI;
+using Vector2 = UnityEngine.Vector2;
 using Vector3 = UnityEngine.Vector3;
 
 public class MovementPlayer : MonoBehaviour
 {
-
+    [Header("General")]
     [SerializeField] private Rigidbody rb;
+    [SerializeField] private Transform orientation;
+    private Vector3 direction;
+
+    [Header("movement")]
     [SerializeField] private float moveSpeed;
-    [SerializeField] private float acceleration;
-    private Vector3 deltaMovement;
+    private Vector2 deltaMovement;
+    [SerializeField] private float groundDrag;
+    [SerializeField] private float airDrag;
+
+    [Header("Ground Check")] 
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private float playerHeaight;
+    [SerializeField] private LayerMask whatIsGround;
+
+    [Header("Jump")] 
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float jumpCooldown;
+    [SerializeField] private float airmultiplier;
+    private bool canJump;
+    
     
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        ResetJump();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        deltaMovement.x = Input.GetAxis("Horizontal");
-        deltaMovement.y = 0;
-        deltaMovement.z = Input.GetAxis("Vertical");
+    void Update() {
+
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeaight * 0.5f + 0.2f, whatIsGround);
+
+        if (isGrounded)
+            rb.drag = groundDrag;
+        else
+            rb.drag = airDrag;
+
+        if (Input.GetButton("Jump") && canJump && isGrounded) {
+            canJump = false;
+            Jump();
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
         
-        Debug.Log(deltaMovement.x);
-        Debug.Log(deltaMovement.z);
-        
-        MovePlayer(deltaMovement * moveSpeed * Time.deltaTime);
+        GetInput();
+        LimitSpeed();
+        MovePlayer();
     }
 
-    void MovePlayer(Vector3 deltaMovement)
-    {
-        rb.MovePosition(transform.position + deltaMovement);
+    void GetInput() {
+        deltaMovement.x = Input.GetAxis("Horizontal");
+        deltaMovement.y = Input.GetAxis("Vertical");
     }
+
+    void MovePlayer() {
+        //  Calculate direction
+        direction = orientation.forward * deltaMovement.y + orientation.right * deltaMovement.x;
+
+        //  on ground
+        if(isGrounded)
+            rb.AddForce(direction.normalized * Time.deltaTime * moveSpeed * 300f, ForceMode.Force);
+        
+        //  in air
+        else if(!isGrounded)
+            rb.AddForce(direction.normalized * Time.deltaTime * moveSpeed * 300f * airmultiplier, ForceMode.Force);
+    }
+
+    void LimitSpeed() {
+
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        
+        //  Limit velocity
+        if (flatVel.magnitude > moveSpeed) {
+            Vector3 limitVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitVel.x, rb.velocity.y, limitVel.z);
+        }
+    }
+
+    void Jump() {
+        //  Reset Y-velocity
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    void ResetJump() {
+        canJump = true;
+    }
+    
+
 }
