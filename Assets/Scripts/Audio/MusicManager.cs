167de8b838d3799b162eb9a755c4998ca0e73073
BirdiePhoenix <felix.william1993@gmail.com>
2023-02-07 11:48:56 +0100

    Flyttade Audio-scripten och gjorde en MusicManager
    Flyttade till Assets-Scripts-Audio

---
+++ Assets/Scripts/Audio/MusicManager.cs
@@ -0,0 +1,133 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD.Studio;
+using FMODUnity;
+
+public class MusicManager : MonoBehaviour
+{
+   public static MusicManager Instance;
+
+   //Bankladdare
+   private void Awake()
+   {
+      BankLoader();
+      if (Instance != null && Instance != this)
+      {
+         Destroy(this);
+      }
+      else
+      {
+         Instance = this;
+      }
+      DontDestroyOnLoad(gameObject);
+   }
+
+   [Header("BankRef")] 
+   [BankRef] public string masterBank;
+
+   public bool loadSampleData;
+
+   private void BankLoader()
+   {
+      Debug.Log("Has the bank loaded? " + RuntimeManager.HasBankLoaded(masterBank));
+      RuntimeManager.LoadBank(masterBank, loadSampleData);
+   }
+
+   
+   //Emitters för bakgrundsmusik
+   [Header("BGM")] 
+   public Emitters eventEmitters;
+   
+   [System.Serializable]
+   
+   public struct Emitters
+   {
+      public StudioEventEmitter bgmMusic;
+   }
+
+   [Header("Puzzle")] 
+   [SerializeField] private EventReference puzzleStinger;
+
+   [SerializeField] private string puzzleParamName;
+   [SerializeField] private float puzzleParamValue;
+
+   public void PlayPuzzle()
+   {
+      RuntimeManager.PlayOneShot(puzzleStinger);
+   }
+    public void PlayFootstep()
+   {
+      RuntimeManager.PlayOneShot(playerFootsteps);
+   }
+   
+   //Player Funktioner Events.
+   [Header("Player")]
+   [SerializeField] private EventReference playerFootsteps;
+   /*
+   [EventRef]
+   public string playerFootsteps;
+   EventInstance footstepInstance;
+   */
+   
+   
+  /* //Styr stinger för när man dör
+   [Header("GameOver")] 
+   [SerializeField] private EventReference gameOverStinger;
+   
+   public void PlayGameOver()
+   {
+      RuntimeManager.PlayOneShot(gameOverStinger);
+   }
+   //Koppla till "Game Over" */
+
+  /*
+   //Styr Combatmusiken
+   private bool inCombat = false;
+   [Header("Combat")] 
+   public float combatWaitTime = 3f;
+
+   [SerializeField] private string combatParamName;
+   [SerializeField] private float combatParamValue = 0f;
+
+   public void Combat()
+   {
+      if (!inCombat)
+      {
+         Debug.Log("Fight MothaFocka!");
+         RuntimeManager.StudioSystem.setParameterByName("Combat", 1);
+      }
+   }*/
+   
+   /*[Header("Sound")]
+      
+      [Header("Ambience")]
+      */
+   
+ 
+   //Play, Stop, Parameter
+   public void Play(StudioEventEmitter emitter)
+   {
+      if (emitter.IsActive == false)
+      {
+         emitter.Play();
+         Debug.Log("Playing!");
+      }
+      else
+      {
+         Debug.Log("Already playing!");
+      }
+   }
+
+   public void Stop(StudioEventEmitter emitter)
+   {
+      emitter.Stop();
+      Debug.Log("Shut up!");
+   }
+
+   public void SetParameter(StudioEventEmitter emitter, string paramName, float paramValue, bool ignorSeek)
+   {
+      emitter.SetParameter(paramName, paramValue, ignorSeek);
+      Debug.Log("Setting parameter.");
+   }
+}
