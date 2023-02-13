21c172f0e118b25f446adfec7419aee80e3cf5a4
Bäverbill <beverbill.gaming@gmail.com>
2023-02-13 11:57:14 +0100

    Required Component för physics object

--- Assets/Scripts/Interactables/PhysicsObjects/PhysicsObject.cs
+++ Assets/Scripts/Interactables/PhysicsObjects/PhysicsObject.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(PhysicsSounds))]
 public class PhysicsObject : Interactable
 {
     float waitOnPickup = 0.2f;
