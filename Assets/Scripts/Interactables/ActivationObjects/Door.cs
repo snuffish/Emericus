402283df6aab0a1ac66c45e46ec485a3b4916ae3
Demian Skoglöv <demian.skoglow@gmail.com>
2023-02-13 16:23:41 +0100

    fixade dörrar

--- Assets/Scripts/Interactables/ActivationObjects/Door.cs
+++ Assets/Scripts/Interactables/ActivationObjects/Door.cs
@@ -2,20 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(AudioSource))]
+
 public class Door : Activators
 {
     [SerializeField] private bool isOpen;
     [SerializeField] private Animator animator;
-    [SerializeField] private AudioClip audioClip;
-    [SerializeField] private AudioSource audioSource;
 
 
     void Start()
     {
         isOpen = false;
-        audioSource = GetComponent<AudioSource>();
-        audioSource.clip = audioClip;
+
     }
 
     public override void Interact()
