0f7e76b99b60ee8f1cdb7d8c4a2b23e469ebe31b
Leon <leonsunmountain@hotmail.com>
2023-02-13 17:23:58 +0100

    Door Script (sound) + Master Bank update
    Door, Master Bank, Leon's test scene & removed unused sound assets

--- Assets/Scripts/Interactables/ActivationObjects/Door.cs
+++ Assets/Scripts/Interactables/ActivationObjects/Door.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
 
 
 public class Door : Activators
 {
     [SerializeField] private bool isOpen;
     [SerializeField] private Animator animator;
+    [SerializeField] private EventReference DOOR_Open;
+    [SerializeField] private EventReference DOOR_Close;
+    [SerializeField] private EventReference DOOR_Open_Puzzle;
 
 
     void Start()
@@ -25,7 +29,12 @@ public class Door : Activators
     public override void ChangeState(bool toState)
     {
         base.ChangeState(toState);
-        if(isActive) animator.SetBool("IsOpen", true);
+        if (isActive) //animator.SetBool("IsOpen", true);
+        { 
+            animator.SetBool("IsOpen", true);
+            AudioManager.Instance.PlayOneShot(DOOR_Open_Puzzle, gameObject);
+        }
+       
         else animator.SetBool("IsOpen", false);
 
     }
