e1cbcee706c3e102b81abb4c2a88d80a3803c845
Demian Skoglöv <demian.skoglow@gmail.com>
2023-03-08 13:55:14 +0100

    Gitignore change med FMOD

---
+++ Assets/Scripts/Audio/UIAudio.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIAudio : MonoBehaviour
+{
+    public void PlayClickSound() {
+        Debug.Log("Played Click Sound");
+    }
+}
