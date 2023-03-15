7c697c2f634caa5c741d0866b64f2c9445f3abce
BirdiePhoenix <felix.william1993@gmail.com>
2023-03-15 16:20:57 +0100

    Voiceline när man dör?
    La in funktioner i PlayerHealth

--- Assets/Scripts/General Components/Health/PlayerHealth.cs
+++ Assets/Scripts/General Components/Health/PlayerHealth.cs
@@ -3,12 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 public class PlayerHealth : Health
 {
+    public struct VoiceSettings
+    {
+        public VoiceAction vAction;
+        public VoiceEvent vEvent;
+        //EmitterTest
+        //public StudioEventEmitter vEmitter;
+        public string keyName;
+        public string paramName;
+        public float paramValue;
+        public bool ignoreSeek;
+        public bool paramGlobal;
+    }
+    
+    [NonReorderable] public VoiceSettings[] voiceSettings;
+    private VLManager vM;
     
     [SerializeField] private SceneManager _sceneManager;
     [SerializeField] private float transistionTime;
+    void Start()
+    {
+        vM = GameObject.Find("VLManager").GetComponent<VLManager>();
+    }
+    
     protected override void Die() {
         Debug.Log("Dead");
         StartCoroutine(_sceneManager.LoadLevel(UnityEngine.SceneManagement.SceneManager.GetActiveScene().handle, transistionTime));
         // GameObject.Find("MusicManager").GetComponent<MusicManager>().PlayGameOver();
+        
+        foreach (VoiceSettings v in voiceSettings)
+        {
+            switch (v.vAction)
+            {
+                case VoiceAction.PlayDialogue:
+                    vM.PlayDialogue(v.vEvent, v.keyName);
+                    vM.dialogueInstance.start();
+                    break;
+                case VoiceAction.SetParameter:
+                    vM.SetParameterVL(v.vEvent, v.paramName, v.paramValue, v.ignoreSeek, v.paramGlobal);
+                    break;
+                default:Debug.Log("Error! No valid action.");
+                    break;
+            }
+        }
     }
 }
