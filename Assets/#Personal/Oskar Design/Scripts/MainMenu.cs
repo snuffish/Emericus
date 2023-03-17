c67d03b1d1c251096389a44bf532f884a6c687a8
BirdiePhoenix <felix.william1993@gmail.com>
2023-03-17 17:50:11 +0100

    Tog bort en parameter när man backtomainmenu

--- Assets/#Personal/Oskar Design/Scripts/MainMenu.cs
+++ Assets/#Personal/Oskar Design/Scripts/MainMenu.cs
@@ -21,7 +21,6 @@ public class MainMenu : MonoBehaviour
     }
 
     public void BackToMainMenu() {
-        RuntimeManager.StudioSystem.setParameterByName(paramRef, paramValue, ignoreSeek);
         UnityEngine.SceneManagement.SceneManager.LoadScene(0);
     }
 
